856d3cada4e38139b756cad6c14a2ba91630f427
Rutvik-C <rutvikchoudhari97@gmail.com>
2022-01-26 16:08:14 +0530

    blogs completed without filters

--- tfgBackend/main/urls.py
+++ tfgBackend/main/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("person/", views.GetPerson.as_view(), name="person"),
-    path("blog/", views.GetBlog.as_view(), name="blog")
+    path('blog/', views.fetch_blogs, name="blog"),
 ]
--- tfgBackend/main/views.py
+++ tfgBackend/main/views.py
@@ -2,21 +2,52 @@ import json
 
 from django.conf import settings
 from django.shortcuts import render
+from django.http import JsonResponse
 
 from .models import *
 
 
 def fetch_blogs(request):
     if request.method == "GET":
+        d = dict(request.GET)
+        filter = d["tag"]
+        print(filter)
+
         blogs = Blog.objects.all()
 
         res = []
-        for blog in blogs:
+        for blog_object in blogs:
+            tags = []
+            for tag in blog_object.tags.all():
+                tmp = {
+                    "tag": tag.name,
+                    "count": tag.count
+                }
+                tags.append(tmp)
+
+            comments = []
+            cmts = BlogComment.objects.filter(blog__pk=blog_object.pk)
+            for comment_object in cmts:
+                tmp = {
+                    "user": comment_object.user.username,
+                    "text": comment_object.comment,
+                    "datePublished": str(comment_object.date_posted)
+
+                }
+                comments.append(tmp)
+
+            scheme = request.is_secure() and "https" or "http"
             tmp = {
-                "id": blog.id,
-                "img": settings.MEDIA_URL + blog.image.url,
-                "author": blog.person.user.username,
-                "title"
-                "description"
-                ""
+                "id": blog_object.id,
+                "img": f'{scheme}://{request.get_host()}' + blog_object.image.url,
+                "author": blog_object.person.username,
+                "title": blog_object.title,
+                "datePublished": str(blog_object.date_posted),
+                "content": blog_object.content,
+                "votes": blog_object.votes,
+                "tags": tags,
+                "comments": comments
             }
+            res.append(tmp)
+
+        return JsonResponse({"data": res})
