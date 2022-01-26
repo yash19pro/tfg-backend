16b9cc8f664432d41adfc107e5664432ca9a8743
yash19_pro <65077864+yash19pro@users.noreply.github.com>
2022-01-27 01:02:53 +0530

    tags feature

--- tfgBackend/main/urls.py
+++ tfgBackend/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('blog/', views.fetch_blogs, name="blog"),
     path('project/', views.fetch_projects, name="project"),
+    path('question/', views.fetch_questions, name="question"),
 ]
--- tfgBackend/main/views.py
+++ tfgBackend/main/views.py
@@ -1,7 +1,4 @@
-import json
-
-from django.conf import settings
-from django.shortcuts import render
+from django.db.models import Q
 from django.http import JsonResponse
 
 from .models import *
@@ -10,7 +7,7 @@ from .models import *
 def fetch_blogs(request):
     if request.method == "GET":
         d = dict(request.GET)
-        filter = d["tag"]
+        filter = d.get('tag', None)
         print(filter)
 
         blogs = Blog.objects.all()
@@ -18,12 +15,18 @@ def fetch_blogs(request):
         res = []
         for blog_object in blogs:
             tags = []
+            ok = False if filter else True
             for tag in blog_object.tags.all():
                 tmp = {
                     "tag": tag.name,
                     "count": tag.count
                 }
+                if filter and tmp['tag'] in filter:
+                    ok = True
                 tags.append(tmp)
+
+            if not ok:
+                continue
 
             comments = []
             cmts = BlogComment.objects.filter(blog__pk=blog_object.pk)
@@ -51,49 +54,109 @@ def fetch_blogs(request):
 
         return JsonResponse({"data": res})
 
+
 def fetch_projects(request):
-    projects = Project.objects.all()
+    if request.method == 'GET':
+        projects = Project.objects.all()
+        d = dict(request.GET)
+        filter = d.get('tag', None)
+        print(filter)
 
-    res = []
-    scheme = request.is_secure() and "https" or "http"
+        res = []
+        scheme = request.is_secure() and "https" or "http"
 
-    for project_object in projects:
-        tags = []
-        for tag in project_object.tags.all():
+        for project_object in projects:
+            tags = []
+            ok = False if filter else True
+            for tag in project_object.tags.all():
+                tmp = {
+                    "tag": tag.name,
+                    "count": tag.count
+                }
+                if filter and tmp['tag'] in filter:
+                    ok = True
+                tags.append(tmp)
+
+            if not ok:
+                continue
+
+            tech_stack = []
+            for stack in project_object.tech_stack.all():
+                tmp = {
+                    "tech": stack.name
+                }
+                tech_stack.append(tmp)
+
+            comments = []
+            cmts = ProjectComment.objects.filter(project__pk=project_object.pk)
+            for comment_object in cmts:
+                tmp = {
+                    "user": comment_object.user.username,
+                    "text": comment_object.comment,
+                    "datePublished": str(comment_object.date_posted)
+                }
+                comments.append(tmp)
+
             tmp = {
-                "tag": tag.name,
-                "count": tag.count
+                "id": project_object.id,
+                "img": f'{scheme}://{request.get_host()}' + project_object.image.url,
+                "author": project_object.person.username,
+                "title": project_object.title,
+                "datePublished": str(project_object.date_posted),
+                "content": project_object.content,
+                "tags": tags,
+                "tech_stack": tech_stack,
+                "comments": comments
             }
-            tags.append(tmp)
+            res.append(tmp)
 
-        tech_stack = []
-        for stack in project_object.tech_stack.all():
+        return JsonResponse({"data": res})
+
+
+def fetch_questions(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        d = dict(request.GET)
+        filter = d.get('tag', None)
+        print(filter)
+
+        res = []
+        scheme = request.is_secure() and "https" or "http"
+
+        for question_object in questions:
+            tags = []
+            ok = False if filter else True
+            for tag in question_object.tags.all():
+                tmp = {
+                    "tag": tag.name,
+                    "count": tag.count
+                }
+                if filter and tmp['tag'] in filter:
+                    ok = True
+                tags.append(tmp)
+
+            if not ok:
+                continue
+
+            answers = []
+            ans = Answer.objects.filter(question__pk=question_object.pk)
+            for answer_object in ans:
+                tmp = {
+                    "person": answer_object.person.username,
+                    "description": answer_object.description,
+                    "datePublished": str(answer_object.date_posted)
+                }
+                answers.append(tmp)
+
             tmp = {
-                "tech": stack.name
+                "id": question_object.id,
+                "img": f'{scheme}://{request.get_host()}' + question_object.image.url,
+                "author": question_object.person.username,
+                "title": question_object.title,
+                "datePublished": str(question_object.date_posted),
+                "tags": tags,
+                "answers": answers
             }
-            tech_stack.append(tmp)
+            res.append(tmp)
 
-        comments = []
-        cmts = ProjectComment.objects.filter(project__pk=project_object.pk)
-        for comment_object in cmts:
-            tmp = {
-                "user": comment_object.user.username,
-                "text": comment_object.comment,
-                "datePublished": str(comment_object.date_posted)
-            }
-            comments.append(tmp)
-
-        tmp = {
-            "id": project_object.id,
-            "img": f'{scheme}://{request.get_host()}' + project_object.image.url,
-            "author": project_object.person.username,
-            "title": project_object.title,
-            "datePublished": str(project_object.date_posted),
-            "content": project_object.content,
-            "tags": tags,
-            "tech_stack": tech_stack,
-            "comments": comments
-        }
-        res.append(tmp)
-    
-    return JsonResponse({"data": res})
+        return JsonResponse({"data": res})
